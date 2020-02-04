51ec6435ce78f97565ac0d2254b02a49a5b2cca4
Troy Goode <troygoode@gmail.com>
2020-02-03 17:59:35 -0800

    remove Savvy tracking script

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,6 @@ import normalize from "styled-normalize";
 
 import ErrorBoundary from "./error-boundary";
 import Navigation from "./navigation";
-import Helmet from "react-helmet";
 
 const GlobalStyle = createGlobalStyle`
   ${normalize}
@@ -28,13 +27,6 @@ const Layout: React.FC = ({ children }) => {
       <GlobalStyle />
       <Navigation />
       {children}
-      {process.env.NODE_ENV === 'production' ? (
-        <Helmet>
-          <script>
-            {"window.savvy=window.savvy||[],savvy.load=function(d,b){window.savvy.orgId=d,window.savvy.config=b=b||{};var e=b.forceSSL||'https:'===document.location.protocol,a=document.createElement('script');a.type='text/javascript',a.async=!0,a.src=(e?'https://savvy-prod.firebaseapp.com/js/generate?script=website_visitors&org_id=':'http://savvy-prod.firebaseapp.com/js/generate?script=website_visitors&org_id=')+d,console.log('a.src',a.src);var c=document.getElementsByTagName('script')[0];c.parentNode.insertBefore(a,c);},savvy.load('cju66UfGwvk8uiyVbwFf');"}
-          </script>
-        </Helmet>
-      ) : null}
     </ErrorBoundary>
   );
 };
