a60c575a3f322635ec26af785223498ebddbdbd6
Justin Michaliga <justin.michaliga@gmail.com>
2020-03-17 11:51:02 -0400

    feat: add checkmark

--- src/components/privacy/feature-table.tsx
+++ src/components/privacy/feature-table.tsx
@@ -4,23 +4,13 @@ import styled from "styled-components";
 import tw from "tailwind.macro";
 
 import colors from "../../colors";
-import { Desktop, Mobile } from "../container";
+// import { Desktop, Mobile } from "../container";
 
-import postmark from "../../images/providers/postmark.svg";
-import messagebird from "../../images/providers/messagebird.svg";
-import onesignal from "../../images/providers/onesignal.svg";
-import fbMessenger from "../../images/providers/fb-messenger.svg";
-import sendgrid from "../../images/providers/sendgrid.svg";
-import twilio from "../../images/providers/twilio.svg";
-import firebase from "../../images/providers/firebase.svg";
-import slack from "../../images/providers/slack.svg";
-import mailgun from "../../images/providers/mailgun.svg";
-import nexmo from "../../images/providers/nexmo.svg";
-import expo from "../../images/providers/expo.svg";
-import msTeams from "../../images/providers/ms-teams.svg";
+import checkmark from "../../images/checkmark.svg";
 
 const FeatureTable = styled.section`
   ${tw`flex flex-row mt-8pt-24 pb-8 md:py-24 px-4 lg:px-0`}
+  color: ${colors.textPrimary};
 `;
 
 const FeatureTableInfo = styled.div`
@@ -144,80 +134,34 @@ const FeatureTableComponent: React.FC = () => {
 
   return (
     <FeatureTable>
-
       <FeatureTableInfo>
-        <Desktop>
           <FeatureTableTable>
+            <tr>
+              <th></th>
+              <th>Standard</th>
+              <th>Enterprise</th>
+            </tr>
+            <tbody className="highlighted">
+            <tr>
+              <td>Subscription Fee</td>
+              <td>Free</td>
+              <td>$5k / mo</td>
+            </tr>
+            <tr>
+              <td>Usage Fee (free of to 10k/mo)</td>
+              <td>per notification</td>
+              <td>per notification</td>
+            </tr>
+            </tbody>
+            {features.map(feat => (
               <tr>
-                <th></th>
-                <th>Standard</th>
-                <th>Enterprise</th>
+                <td>{feat.label}</td>
+                <td>{feat.standard ? <img src={checkmark} /> : " "}</td>
+                <td>{feat.enterprise ? <img src={checkmark} /> : " "}</td>
               </tr>
-              <tbody className="highlighted">
-              <tr>
-                <td>Subscription Fee</td>
-                <td>Free</td>
-                <td>$5k / mo</td>
-              </tr>
-              <tr>
-                <td>Usage Fee (free of to 10k/mo)</td>
-                <td>per notification</td>
-                <td>per notification</td>
-              </tr>
-              </tbody>
-              {features.map(feat => (
-                <tr>
-                  <td>{feat.label}</td>
-                  <td>{feat.standard ? "+" : "-"}</td>
-                  <td>{feat.enterprise ? "+" : "-"}</td>
-                </tr>
-              ))}
-
+            ))}
           </FeatureTableTable>
-        </Desktop>
-        <Mobile>
-          <FeatureTableTable>
-              <tr>
-              <th>Email</th>
-                <th>SMS</th>
-              </tr>
-              <tr>
-              <td><img src={postmark} alt="Postmark" /></td>
-                <td><img src={messagebird} alt="MessageBird" /></td>
-              </tr>
-              <tr>
-              <td><img src={sendgrid} alt="SendGrid" /></td>
-                <td><img src={twilio} alt="Twilio" /></td>
-              </tr>
-              <tr>
-              <td><img src={mailgun} alt="Mailgun" /></td>
-                <td><img src={nexmo} alt="nexmo" /></td>
-              </tr>
-          </FeatureTableTable>
-          <FeatureTableTable>
-              <tr>
-                <th>Push</th>
-                <th>Direct Message</th>
-              </tr>
-              <tr>
-                <td><img src={onesignal} alt="OneSignal" /></td>
-                <td><img src={fbMessenger} alt="FB Messenger" /></td>
-              </tr>
-              <tr>
-                <td><img src={firebase} alt="Firebase" /></td>
-                <td><img src={slack} alt="Slack" /></td>
-              </tr>
-              <tr>
-                <td><img src={expo} alt="Expo" /></td>
-                <td><img src={msTeams} alt="MS Teams" /></td>
-              </tr>
-          </FeatureTableTable>
-        </Mobile>
-        <div style={{marginTop: 32}}>
-          <a href="#">Other Integrations</a>
-        </div>
       </FeatureTableInfo>
-
     </FeatureTable>
   );
 };
