8fdca57bf37953cb8436ea5e83153d10e672510d
Justin Michaliga <justin.michaliga@gmail.com>
2020-03-25 18:56:39 -0400

    rm comments

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,11 +38,8 @@ exports.createPages = async ({ graphql, actions }) => {
       path: node.fields.slug,
       component: path.resolve(`./src/templates/blog.tsx`),
       context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
         slug: node.fields.slug,
       },
     })
   })
 }
-
