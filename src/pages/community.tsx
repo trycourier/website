feea9c1c0d2c6de762297c8b423c856369674ffe
Justin Michaliga <justin.michaliga@gmail.com>
2020-03-26 12:17:55 -0400

    tags + outlines

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { Link } from "gatsby";
 import Simple from "../templates/simple";
 
-import { ArticleCard, ArticleImage, ArticleList, ArticleScreen } from "../components/community/articles";
+import {
+  ArticleCard,
+  ArticleImage,
+  ArticleList,
+  ArticleScreen,
+  ArticleSearch,
+} from "../components/community/articles";
 import SearchInput from "../components/community/search-input";
 import Tag from "../components/community/tag";
 
@@ -21,19 +27,17 @@ const articles = [
   },
   {
     image: {
-      src: "https://placekitten.com/220/160",
+      src: "https://placekeanu.com/220/160",
       desc: "Sunset in the mountains",
     },
-    title: "Notification Design Studio",
+    title: "How we organize our jobs for our new customers and how we satisfy them.",
     author: "Name of Person",
-    published: "Date/Time",
-    html: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-    Voluptatibus quia, nulla! Maiores et perferendis eaque,
-    exercitationem praesentium nihil.`,
+    published: "February 2md, 2021",
+    html: `Ut auctor ligula id aliquam sollicitudin. Maecenas tincidunt nisl et dignissim dapibus. Etiam eget varius neque. Donec et dapibus diam, aliquam egestas quam. Curabitur condimentum nibh non augue facilisis, vitae pharetra metus consequat. Donec ac urna ac mauris commodo pharetra ut ac mi. `,
   },
   {
     image: {
-      src: "https://placekitten.com/220/160",
+      src: "http://www.fillmurray.com/220/160",
       desc: "Sunset in the mountains",
     },
     title: "Notification Design Studio",
@@ -45,10 +49,11 @@ const articles = [
   },
 ];
 
+const tags = [{ label: "Long Tag" }, { label: "Tag" }];
+
 const Community: React.FC = () => {
   return (
     <Simple title="Community">
-
       <h1>All Articles</h1>
       <p>Feel free to share our content.</p>
       <ArticleScreen>
@@ -56,7 +61,6 @@ const Community: React.FC = () => {
           {articles.map(article => (
             <ArticleCard>
               <ArticleImage src={article.image.src} alt={article.image.desc} />
-
               <div className="px-4">
                 <h4 className="font-bold text-xl py-0 mt-0 mb-2">
                   {article.title}
@@ -74,10 +78,14 @@ const Community: React.FC = () => {
           ))}
         </ArticleList>
         <ArticleSearch>
-            <SearchInput />
-
-            <Tag>Long Tag</Tag>
-            <Tag>Tag</Tag>
+          <SearchInput />
+          {tags.map(tag => (
+            <div
+              style={{ width: "100%", textAlign: "right", margin: "16px 0px" }}
+            >
+              <Tag>{tag.label}</Tag>
+            </div>
+          ))}
         </ArticleSearch>
       </ArticleScreen>
     </Simple>
