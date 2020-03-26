9d97376f6ce394fdf6508576e7aefd6ee52e6661
Justin Michaliga <justin.michaliga@gmail.com>
2020-03-26 12:00:50 -0400

    export out article components

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -1,51 +1,10 @@
 import React from "react";
 import { Link } from "gatsby";
-import styled from "styled-components";
-import tw from "tailwind.macro";
-import colors from "../colors";
 import Simple from "../templates/simple";
 
+import { ArticleCard, ArticleImage, ArticleList, ArticleScreen } from "../components/community/articles";
 import SearchInput from "../components/community/search-input";
 import Tag from "../components/community/tag";
-
-const ArticleScreen = styled.div`
-  ${tw`flex`}
-`;
-
-const ArticleList = styled.div`
-  ${tw`w-3/4 mr-16`}
-`;
-
-const ArticleSearch = styled.div`
-  ${tw`w-1/4`}
-`;
-
-const ArticleCard = styled.div`
-  ${tw`w-full flex my-8`}
-  & h4 {
-    font-size: 22px;
-    font-weight: 400;
-    color: ${colors.textPrimary};
-    margin: 0;
-  }
-  & div.posted {
-    font-size: 14px;
-    padding-top: 8px;
-    & strong {
-      color: ${colors.textPrimary};
-    }
-  }
-  & p.excerpt {
-    font-size: 14px;
-    color: ${colors.textPrimary};
-  }
-`;
-
-const ArticleImage = styled.img`
-  ${tw`w-1/6 flex mr-4`}
-  border-radius: 10px;
-  min-width: 220px;
-`;
 
 const articles = [
   {
