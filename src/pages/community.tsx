1d3ca3221bb7f7fc62358497bcbefc58e6395224
Justin Michaliga <justin.michaliga@gmail.com>
2020-03-26 11:57:41 -0400

    community components

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -2,41 +2,125 @@ import React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
 import tw from "tailwind.macro";
-
+import colors from "../colors";
 import Simple from "../templates/simple";
 
-const EventCard = styled.div`
-  ${tw`max-w-sm rounded overflow-hidden shadow-lg p-4`}
+import SearchInput from "../components/community/search-input";
+import Tag from "../components/community/tag";
+
+const ArticleScreen = styled.div`
+  ${tw`flex`}
 `;
+
+const ArticleList = styled.div`
+  ${tw`w-3/4 mr-16`}
+`;
+
+const ArticleSearch = styled.div`
+  ${tw`w-1/4`}
+`;
+
+const ArticleCard = styled.div`
+  ${tw`w-full flex my-8`}
+  & h4 {
+    font-size: 22px;
+    font-weight: 400;
+    color: ${colors.textPrimary};
+    margin: 0;
+  }
+  & div.posted {
+    font-size: 14px;
+    padding-top: 8px;
+    & strong {
+      color: ${colors.textPrimary};
+    }
+  }
+  & p.excerpt {
+    font-size: 14px;
+    color: ${colors.textPrimary};
+  }
+`;
+
+const ArticleImage = styled.img`
+  ${tw`w-1/6 flex mr-4`}
+  border-radius: 10px;
+  min-width: 220px;
+`;
+
+const articles = [
+  {
+    image: {
+      src: "https://placekitten.com/220/160",
+      desc: "Sunset in the mountains",
+    },
+    title: "Notification Design Studio",
+    author: "Name of Person",
+    published: "Date/Time",
+    html: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Voluptatibus quia, nulla! Maiores et perferendis eaque,
+    exercitationem praesentium nihil.`,
+  },
+  {
+    image: {
+      src: "https://placekitten.com/220/160",
+      desc: "Sunset in the mountains",
+    },
+    title: "Notification Design Studio",
+    author: "Name of Person",
+    published: "Date/Time",
+    html: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Voluptatibus quia, nulla! Maiores et perferendis eaque,
+    exercitationem praesentium nihil.`,
+  },
+  {
+    image: {
+      src: "https://placekitten.com/220/160",
+      desc: "Sunset in the mountains",
+    },
+    title: "Notification Design Studio",
+    author: "Name of Person",
+    published: "Date/Time",
+    html: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Voluptatibus quia, nulla! Maiores et perferendis eaque,
+    exercitationem praesentium nihil.`,
+  },
+];
 
 const Community: React.FC = () => {
   return (
     <Simple title="Community">
-      <h1>Community</h1>
-      <h2>Blog</h2>
-      <hr />
-      <EventCard>
-        <img
-          className="w-full"
-          src="https://placekitten.com/400/200"
-          alt="Sunset in the mountains"
-        />
-        <div className="px-6 py-4">
-          <h4 className="font-bold text-xl mb-2">Notification Design Studio</h4>
-          <div className="text-sm mb-2">
-            posted by <strong>Name of Person</strong> on{" "}
-            <strong>Date and Time</strong>
-          </div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
-          </p>
-        </div>
-        <div className="px-6 py-4">
-          <Link>Read More</Link>
-        </div>
-      </EventCard>
+
+      <h1>All Articles</h1>
+      <p>Feel free to share our content.</p>
+      <ArticleScreen>
+        <ArticleList>
+          {articles.map(article => (
+            <ArticleCard>
+              <ArticleImage src={article.image.src} alt={article.image.desc} />
+
+              <div className="px-4">
+                <h4 className="font-bold text-xl py-0 mt-0 mb-2">
+                  {article.title}
+                </h4>
+                <div className="posted">
+                  Posted by <strong>{article.author}</strong> on{" "}
+                  <strong>{article.published}</strong>
+                </div>
+                <p className="excerpt">{article.html}</p>
+                <div className="px-6 py-4">
+                  <Link>Read More</Link>
+                </div>
+              </div>
+            </ArticleCard>
+          ))}
+        </ArticleList>
+        <ArticleSearch>
+            <SearchInput />
+
+            <Tag>Long Tag</Tag>
+            <Tag>Tag</Tag>
+        </ArticleSearch>
+      </ArticleScreen>
     </Simple>
   );
 };
