94fe66fa7258e5307f918192d10fa62094c040b8
Danny Douglass <266481+DannyDouglass@users.noreply.github.com>
2020-10-20 16:33:43 -0700

    convert register links to app.courier.com

--- src/links.ts
+++ src/links.ts
@@ -2,8 +2,8 @@ export const githubSignUpUrl =
   "https://courier.auth.us-east-1.amazoncognito.com/oauth2/authorize?identity_provider=Github&redirect_uri=https://trycourier.app/login/callback&response_type=CODE&client_id=5f4fmec2qnuscp89qbt8nsuftj&scope=aws.cognito.signin.user.admin%20email%20openid%20profile";
 export const googleSignUpUrl =
   "https://courier.auth.us-east-1.amazoncognito.com/oauth2/authorize?identity_provider=Google&redirect_uri=https://www.trycourier.app/login/callback&response_type=CODE&client_id=5f4fmec2qnuscp89qbt8nsuftj&scope=aws.cognito.signin.user.admin%20email%20openid%20profile";
-export const emailSignUpUrl = "https://www.trycourier.app/register/email";
-export const signUpUrl = "https://www.trycourier.app/register/";
+export const emailSignUpUrl = "https://app.courier.com/register/email";
+export const signUpUrl = "https://app.courier.com/register/";
 
 type SignUpType = "GitHub" | "Google" | "Email";
 
