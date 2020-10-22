27f969b5b324f1a9a7e165819057849d55b2b5e9
Danny Douglass <266481+DannyDouglass@users.noreply.github.com>
2020-10-22 12:32:54 -0700

    few more places we still have https://www.trycourier.app

--- src/links.ts
+++ src/links.ts
@@ -1,7 +1,7 @@
 export const githubSignUpUrl =
-  "https://courier.auth.us-east-1.amazoncognito.com/oauth2/authorize?identity_provider=Github&redirect_uri=https://trycourier.app/login/callback&response_type=CODE&client_id=5f4fmec2qnuscp89qbt8nsuftj&scope=aws.cognito.signin.user.admin%20email%20openid%20profile";
+  "https://courier.auth.us-east-1.amazoncognito.com/oauth2/authorize?identity_provider=Github&redirect_uri=https://app.courier.com/login/callback&response_type=CODE&client_id=5f4fmec2qnuscp89qbt8nsuftj&scope=aws.cognito.signin.user.admin%20email%20openid%20profile";
 export const googleSignUpUrl =
-  "https://courier.auth.us-east-1.amazoncognito.com/oauth2/authorize?identity_provider=Google&redirect_uri=https://www.trycourier.app/login/callback&response_type=CODE&client_id=5f4fmec2qnuscp89qbt8nsuftj&scope=aws.cognito.signin.user.admin%20email%20openid%20profile";
+  "https://courier.auth.us-east-1.amazoncognito.com/oauth2/authorize?identity_provider=Google&redirect_uri=https://app.courier.com/login/callback&response_type=CODE&client_id=5f4fmec2qnuscp89qbt8nsuftj&scope=aws.cognito.signin.user.admin%20email%20openid%20profile";
 export const emailSignUpUrl = "https://app.courier.com/register/email";
 export const signUpUrl = "https://app.courier.com/register/";
 
