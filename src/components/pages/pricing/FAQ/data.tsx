26b98ffc340ef37c521bf8a3607d3670fce4f352
Nick Gottlieb <ngottlieb1@gmail.com>
2021-06-02 16:46:38 -0700

    Update data.tsx (#93)

--- src/components/pages/pricing/FAQ/data.tsx
+++ src/components/pages/pricing/FAQ/data.tsx
@@ -1,22 +1,10 @@
 export default [
     {
-        question: "Is there a free trial of the paid plans?",
-        answer: "Yes! To start a free trial of any of our paid plans,  or chat with us."
-    },
-    {
-        question: "How does your pricing work?",
-        answer: "All of Courier’s paid plans have a fixed platform fee and included usage. If you exceed your included usage, you will be charged an overage fee per additional notification, which is variable depending on your plan type."
-    },
-    {
         question: "How do you calculate usage?",
         answer: "Usage is calculated based on the number of notifications successfully delivered by Courier. Notifications sent using your test API keys will count toward that month’s usage."
     },
     {
-        question: "How do you calculate usage?",
-        answer: "No, you are responsible for any services that you integrate with Courier."
-    },
-    {
-        question: "How do you calculate usage?",
-        answer: "To upgrade to the Starter or Growth plan, just visit the billing page in the app. Note that only the account owner can upgrade your account. If you’d like to upgrade to the Enterprise, book a time with our sales team."
+        question: "Do you offer volume discounts?",
+        answer: "Yes, we offer volume discounts as part of our Enterprise tier. Please reach out to learn more."
     },
 ];
