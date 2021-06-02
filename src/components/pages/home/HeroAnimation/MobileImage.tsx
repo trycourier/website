88ce31c6ba500bd6aaa185ecdde7258631af7f36
Pravin <pdaryani@gmail.com>
2021-06-02 21:23:35 +0200

    Update mobile hero static image

--- src/components/pages/home/HeroAnimation/MobileImage.tsx
+++ src/components/pages/home/HeroAnimation/MobileImage.tsx
@@ -2,7 +2,7 @@ import { Image, Box } from '@chakra-ui/react';
 
 const HomeHeroMobileImage = () => (
     <Box display={{base: "block", xl: "none"}}>
-        <Image src={'/images/pages/home/dashboard/dashboard.png'} mx='auto' mt={{base: "-180px", xl: '-500px'}} padding='0 20px'/>    
+        <Image src={'/images/pages/home/hero/mobile-static.png'} mx='auto' mt={{base: "-180px", xl: '-500px'}} padding='0 20px'/>    
     </Box>    
 );
 
