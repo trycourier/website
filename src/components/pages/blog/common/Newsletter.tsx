6e5b2bbe6e7bd176026444ff2d09ba829fcde083
Pravin <pdaryani@gmail.com>
2021-06-10 21:26:22 +0200

    Fix HS integration

--- src/components/pages/blog/common/Newsletter.tsx
+++ src/components/pages/blog/common/Newsletter.tsx
@@ -1,31 +1,63 @@
 import React, { useState, useRef } from 'react';
 import { InputGroup, Input, InputRightElement, Alert, AlertIcon, Box, FormControl, Spinner } from '@chakra-ui/react';
 import PurpleBgButton from 'components/buttons/PurpleBg';
+import cookies from 'js-cookie';
+
+function makeFormObj({email, hutk}: {email: string, hutk: string}) {
+    const data = {
+        "submittedAt": new Date().valueOf(),
+        "fields": [
+            {
+            "name": "email",
+            "value": email
+            }
+        ],
+        "context": {
+            "hutk": hutk,
+            "pageUri": window.location.href,
+            "pageName": document.title
+        },
+        "legalConsentOptions":{
+            "consent":{
+            "consentToProcess":true,
+            "text":"I agree to allow Courier to store and process my personal data.",
+            "communications":[
+                {
+                "value":true,
+                "subscriptionTypeId":999,
+                "text":"I agree to receive marketing communications from Courier."
+                }
+            ]
+            }
+        }
+    }
+    return data;
+}
 
 const BlogNewsletter = () => {
     const [ subscriptionSuccess, setSubscriptionSuccess ] = useState(false);
     const [ isLoading, setIsLoading ] = useState(false);
     const emailRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+    const HS_FORM_ENDPOINT = `https://api.hsforms.com/submissions/v3/integration/submit/6196736/2d4975ec-6ff8-4609-b2d9-18fe9c701e5b`;
 
     async function handleSubscription() {
         const emailFieldValid = emailRef.current.reportValidity();
         if(emailFieldValid) {
             setIsLoading(true);
             const email = emailRef.current.value;
-            // let hutk = cookies.get('hubspotutk');
-            // if (!hutk) {
-            //     //TODO: figure a solution when cookie not available
-            //     hutk = "0357bb413489ee6bfdeda28e0566a639"
-            // };
-
-            await fetch(`/api/newsletter-subscribe`, {
+            let hutk = cookies.get('hubspotutk');
+            if (!hutk) {
+                //fallback HS cookie when not available
+                hutk = "0357bb413489ee6bfdeda28e0566a639"
+            };
+            
+            const submitFormObj = makeFormObj({email, hutk});
+            await fetch(HS_FORM_ENDPOINT, {
                 method: 'POST',
-                body: JSON.stringify({
-                    email,
-                    // hutk, 
-                    pageUri: window.location.href,
-                    pageName: document.title
-                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(submitFormObj),
             });
             setIsLoading(false);
             setSubscriptionSuccess(true);
