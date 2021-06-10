f58c5dba5f16ee3718c56fac7cbcc62674effe1a
Pravin <pdaryani@gmail.com>
2021-06-10 19:33:59 +0200

    Add blog search

--- src/components/pages/blog/common/Menu/Mobile.tsx
+++ src/components/pages/blog/common/Menu/Mobile.tsx
@@ -1,9 +1,9 @@
 import { Flex, Select } from '@chakra-ui/react';
-import SearchButton from './SearchButton';
+import Search from './Search';
 
 const Mobile = ({data}: {data: any[]}) => (
     <Flex p="0 20px" w={{base: "100%", md: "70%", lg: "60%"}} justify="flex-start" mb={4} float="right">
-        <SearchButton mr={4} />
+        <Search mr={4} />
         <Select>
             {
                 data.map(menu => (
