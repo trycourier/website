464bd042e3436b89934e2aeff3557b13f17cfad3
pdaryani <43791027+pdaryani@users.noreply.github.com>
2021-06-22 23:31:00 +0200

    Redirects + CSP via next.config.js (#109)
    * Remove marketing redirects from _redirects
    
    * Manage redirects via next.config.js
    
    * Manage headers via next.config.js instead of netlify.toml

---
+++ redirects.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        source: '/sedaily',
+        destination: '/?utm_medium=podcast&utm_source=sedaily&utm_campaign=se-daily-podcast-q1',
+        permanent: true,
+    },
+    {
+        source: '/codestory',
+        destination: '/?utm_campaign=Podcasts&utm_source=code-story&utm_medium=roll-ad',
+        permanent: true,
+    },
+    {
+        source: '/hackabull',
+        destination: 'https://app.courier.com/?utm_medium=hackathon&utm_source=hackbull-usf&utm_campaign=developer-relations',
+        permanent: true,
+    },
+    {
+        source: '/bitcamp',
+        destination: 'https://app.courier.com/?utm_campaign=Developer%20Relations&utm_source=bitcamp&utm_medium=sign-up',
+        permanent: true,
+    },
+    {
+        source: '/tohacks',
+        destination: 'https://app.courier.com/?utm_campaign=Developer%20Relations&utm_source=tohacks&utm_medium=sign-up',
+        permanent: true,
+    },
+    {
+        source: '/careers',
+        destination: 'https://jobs.lever.co/trycourier/',
+        permanent: true,
+    },
+] 
