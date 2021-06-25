e3232679a1cdd86b893accae7062e6a2566f65da
Nick Gottlieb <ngottlieb1@gmail.com>
2021-06-25 14:27:56 -0600

    Update Feed.tsx (#112)
    * Update Feed.tsx
    
    * Update Tag.tsx
    
    * Update Feed.tsx
    
    * Update data.tsx
    
    * Update Feed.tsx
    
    * Update Tag.tsx
    
    * Update Feed.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx

--- src/components/pages/blog/home/FeaturedBlock/index.tsx
+++ src/components/pages/blog/home/FeaturedBlock/index.tsx
@@ -3,7 +3,7 @@ import MainArticle from '../../common/FeaturedBlock/MainArticle';
 import ContentGrid from '../ContentGrid';
 
 const FeaturedBlock = ({items}: {items: any[]}) => (
-    <Box>
+    <Box pt="20px">
         <MainArticle item={items[0]} />  
         <ContentGrid items={items.slice(1, 4)} />      
     </Box>
--- src/components/pages/blog/home/Feed.tsx
+++ src/components/pages/blog/home/Feed.tsx
@@ -1,11 +1,9 @@
 import { Box } from '@chakra-ui/react';
-import CourierLive from './CourierLive';
 import FeaturedBlock from './FeaturedBlock';
 
 const Feed = ({feedContent}: {feedContent: any}) => (
     <Box w={{base: "100%", xl: "80%"}} ml={{base: 0, xl: "62px"}} p={{base: "0 20px", xl: 0}}>
         <FeaturedBlock items={feedContent.topFeatured} />
-        <CourierLive items={feedContent.courierLive} />
         <FeaturedBlock items={feedContent.bottomFeatured} />
     </Box>
 );
