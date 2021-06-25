6317cbca3b5de929b082206fa1879a490ae4041e
Nick Gottlieb <ngottlieb1@gmail.com>
2021-06-25 13:39:31 -0600

    Update Feed.tsx

--- src/components/pages/blog/home/Feed.tsx
+++ src/components/pages/blog/home/Feed.tsx
@@ -5,7 +5,6 @@ import FeaturedBlock from './FeaturedBlock';
 const Feed = ({feedContent}: {feedContent: any}) => (
     <Box w={{base: "100%", xl: "80%"}} ml={{base: 0, xl: "62px"}} p={{base: "0 20px", xl: 0}}>
         <FeaturedBlock items={feedContent.topFeatured} />
-        <FeaturedBlock items={feedContent.bottomFeatured} />
     </Box>
 );
 
