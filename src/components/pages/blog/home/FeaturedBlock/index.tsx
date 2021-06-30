4cd2076ea8263dd6443e2c2cf848fb7e8a94a8f0
Pravin <pdaryani@gmail.com>
2021-06-30 18:08:10 +0200

    Blog home spacing update

--- src/components/pages/blog/home/FeaturedBlock/index.tsx
+++ src/components/pages/blog/home/FeaturedBlock/index.tsx
@@ -3,7 +3,7 @@ import MainArticle from '../../common/FeaturedBlock/MainArticle';
 import ContentGrid from '../ContentGrid';
 
 const FeaturedBlock = ({items}: {items: any[]}) => (
-    <Box pt="20px">
+    <Box>
         <MainArticle item={items[0]} />  
         <ContentGrid items={items.slice(1, 4)} />      
     </Box>
