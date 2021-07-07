c390f9fd17f5d4f868788dc785d94875b97995b2
Nick Gottlieb <ngottlieb1@gmail.com>
2021-07-07 15:04:04 -0600

    Update Hero.tsx (#118)
    * Update Hero.tsx
    
    * Update Hero.tsx

--- src/components/pages/blog/home/Hero.tsx
+++ src/components/pages/blog/home/Hero.tsx
@@ -3,8 +3,8 @@ import { Heading, Box, Text, Flex } from '@chakra-ui/react';
 const Hero = () => (
     <Flex color="secondary.dark" mt={{base: "132px", lg: "142px", xl: "192px"}} w={"100%"}>
         <Box mx='auto' textAlign='center' p="0 20px">
-            <Heading variant="h2">Latest news and updates</Heading>
-            <Text textAlign="center" mt={4} variant="body1">Helpful tips, live streams with amazing people and more...</Text>
+            <Heading variant="h2">Welcome to the Courier Engineering Blog</Heading>
+            <Text textAlign="center" mt={4} variant="body1">Interesting topics and learnings in software development.</Text>
         </Box>
     </Flex>            
 );
