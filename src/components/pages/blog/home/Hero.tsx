04a72a95963372e35d43f174e2ac2007aa9c5c78
Nick Gottlieb <ngottlieb1@gmail.com>
2021-07-07 15:03:54 -0600

    Update Hero.tsx

--- src/components/pages/blog/home/Hero.tsx
+++ src/components/pages/blog/home/Hero.tsx
@@ -3,7 +3,7 @@ import { Heading, Box, Text, Flex } from '@chakra-ui/react';
 const Hero = () => (
     <Flex color="secondary.dark" mt={{base: "132px", lg: "142px", xl: "192px"}} w={"100%"}>
         <Box mx='auto' textAlign='center' p="0 20px">
-            <Heading variant="h2">Welcome to the Courier Developer Blog!</Heading>
+            <Heading variant="h2">Welcome to the Courier Engineering Blog</Heading>
             <Text textAlign="center" mt={4} variant="body1">Interesting topics and learnings in software development.</Text>
         </Box>
     </Flex>            
