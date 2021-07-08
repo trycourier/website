7aadd499567d57d270b4d779026602d649114d35
Vahagn Aharonian <vahagn.aharonian@toptal.com>
2021-07-08 22:55:18 +0400

    Implement blog markdown content (#119)
    * Implement blog markdown content
    
    * Fix rendering of richtext lists

--- src/pages/api/blog-search.ts
+++ src/pages/api/blog-search.ts
@@ -1,27 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
-const contentful = require('contentful')
-const client = contentful.createClient({
-    space: `${process.env.CONTENTFUL_SPACE_ID}`,
-    accessToken: `${process.env.CONTENTFUL_API_KEY}`
-});
+import client from "../../scripts/contentfulClient";
 
-export default async(req: NextApiRequest, res: NextApiResponse) => {
-    const {query} = req.query;
-    const response = await client.getEntries({
-        query,
-        content_type: "post", 
-        limit: 1000 
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { query } = req.query;
+  const response = await client.getEntries({
+    query,
+    content_type: "post",
+    limit: 1000,
+  });
+  const rawItems = response.items;
+  const returnItems = [];
+  for (let index = 0; index < rawItems.length; index++) {
+    const item = rawItems[index];
+    const { title, slug, tags } = item.fields;
+    const tag = tags[0].fields.name;
+    returnItems.push({
+      title,
+      slug,
+      tag,
     });
-    const rawItems = response.items;
-    const returnItems = [];
-    for (let index = 0; index < rawItems.length; index++) {
-        const item = rawItems[index];
-        const { title, slug, tags } = item.fields;
-        const tag = tags[0].fields.name;
-        returnItems.push({
-            title, slug, tag
-        })
-    }
-    res.statusCode = 200;
-    return res.json({ results: returnItems });
+  }
+  res.statusCode = 200;
+  return res.json({ results: returnItems });
 };
--- src/scripts/GetBlogPost.tsx
+++ src/scripts/GetBlogPost.tsx
@@ -1,21 +1,30 @@
-import GetSimilarBlogs from './GetSimilarBlogs';
+import client from "./contentfulClient";
+import GetSimilarBlogs from "./GetSimilarBlogs";
 
 interface Props {
-    slug: string;
-    isPreview?: boolean;
+  slug: string;
+  isPreview?: boolean;
 }
 
-async function getBlogPostId({slug, isPreview}: Props) {
-    const response = await fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${isPreview ? process.env.CONTENTFUL_PREVIEW_API_KEY : process.env.CONTENTFUL_API_KEY}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
+async function getBlogPostId({ slug, isPreview }: Props) {
+  const response = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${
+          isPreview
+            ? process.env.CONTENTFUL_PREVIEW_API_KEY
+            : process.env.CONTENTFUL_API_KEY
+        }`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
         query: `
         query postEntryQuery {
-            postCollection(where: {slug:"${slug}"}, preview: ${isPreview ? true : false}) {
+            postCollection(where: {slug:"${slug}"}, preview: ${
+          isPreview ? true : false
+        }) {
               items {
                 sys {
                     id
@@ -23,22 +32,35 @@ async function getBlogPostId({slug, isPreview}: Props) {
               }
             }
           }
-        `
-        }),
-    });
-    const data = await response.json();
-    const blogPostId = data.data.postCollection.items[0].sys.id;
-    return blogPostId;
+        `,
+      }),
+    }
+  );
+  const data = await response.json();
+  const blogPostId = data.data.postCollection.items[0].sys.id;
+  return blogPostId;
 }
 
-async function getBlogPostDetails({blogPostId, isPreview}: {blogPostId: string, isPreview?: boolean}) {
-    const response = await fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${isPreview ? process.env.CONTENTFUL_PREVIEW_API_KEY :  process.env.CONTENTFUL_API_KEY}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
+async function getBlogPostDetails({
+  blogPostId,
+  isPreview,
+}: {
+  blogPostId: string;
+  isPreview?: boolean;
+}) {
+  const response = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${
+          isPreview
+            ? process.env.CONTENTFUL_PREVIEW_API_KEY
+            : process.env.CONTENTFUL_API_KEY
+        }`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
         query: `
             query postEntryQuery {
                 post(id: "${blogPostId}" preview: ${isPreview ? true : false}) {
@@ -64,6 +86,7 @@ async function getBlogPostDetails({blogPostId, isPreview}: {blogPostId: string, 
                         title
                         url
                     }
+                    contentMd
                     content {
                         json
                         links {
@@ -90,54 +113,79 @@ async function getBlogPostDetails({blogPostId, isPreview}: {blogPostId: string, 
                     }
                 }
             }
-        `
-        }),
-    });
-    const data = await response.json();
-    let blogPostDetails = data.data.post;
-    const blogImages:any = {};
-    const imagesArrRaw = data.data.post.content.links.assets.block;
-    for (let index = 0; index < imagesArrRaw.length; index++) {
-        const thisImage = imagesArrRaw[index];
-        const { url, height, width, title } = thisImage
-        blogImages[thisImage.sys.id] = {
-            url, height, width, title
-        }   
+        `,
+      }),
     }
-    blogPostDetails.images = blogImages;
+  );
+  const data = await response.json();
+  let blogPostDetails = data.data.post;
+  const blogImages: any = {};
+  const imagesArrRaw = data.data.post.content.links.assets.block;
+  for (let index = 0; index < imagesArrRaw.length; index++) {
+    const thisImage = imagesArrRaw[index];
+    const { url, height, width, title } = thisImage;
+    blogImages[thisImage.sys.id] = {
+      url,
+      height,
+      width,
+      title,
+    };
+  }
+  blogPostDetails.images = blogImages;
 
-    const blogSnippets:any = {};
-    const codeSnippetsArrRaw = data.data.post.content.links.entries.block;
-    for (let index = 0; index < codeSnippetsArrRaw.length; index++) {
-        const thisSnippet = codeSnippetsArrRaw[index];
-        blogSnippets[thisSnippet.sys.id] = {
-            title: thisSnippet.title,
-            language: thisSnippet.language,
-            code: thisSnippet.code
-        };   
-    }
-    blogPostDetails.snippets = blogSnippets;
+  const blogSnippets: any = {};
+  const codeSnippetsArrRaw = data.data.post.content.links.entries.block;
+  for (let index = 0; index < codeSnippetsArrRaw.length; index++) {
+    const thisSnippet = codeSnippetsArrRaw[index];
+    blogSnippets[thisSnippet.sys.id] = {
+      title: thisSnippet.title,
+      language: thisSnippet.language,
+      code: thisSnippet.code,
+    };
+  }
+  blogPostDetails.snippets = blogSnippets;
 
-    return blogPostDetails;
+  // Extract assets from markdown content and fetch their width/height
+  const imageRegex = /\(\/\/images\.ctfassets\.net\/(?:.+?)\/(.+?)\/(?:.+?)\/(?:.+?)\)/g;
+  const matches = [...(blogPostDetails.contentMd || "").matchAll(imageRegex)];
+  const assetIds = Array.from(new Set(matches.map((match) => match[1])));
+  const assets = await Promise.all(
+    assetIds.map((assetId) => client.getAsset(assetId))
+  );
+  const imageDimensions = assets.reduce(
+    (obj, asset) => ({
+      ...obj,
+      [asset.fields.file.url]: asset.fields.file.details.image,
+    }),
+    {}
+  );
+  blogPostDetails.imageDimensions = imageDimensions;
+
+  return blogPostDetails;
 }
 
 async function getMostPopularBlogs() {
-    const response = await fetch(`https://s3.amazonaws.com/courier.com/most-popular-blogs.json`);
-    const data = await response.json();
-    const mostPopularBlogs = data.splice(0, 4);
-    return mostPopularBlogs;
+  const response = await fetch(
+    `https://s3.amazonaws.com/courier.com/most-popular-blogs.json`
+  );
+  const data = await response.json();
+  const mostPopularBlogs = data.splice(0, 4);
+  return mostPopularBlogs;
 }
 
-const GetBlogPost = async ({slug, isPreview}: Props) => {
-    const blogPostId = await getBlogPostId({slug, isPreview});
-    const blogPostDetails = await getBlogPostDetails({blogPostId, isPreview});
-    const mostPopularBlogs = await getMostPopularBlogs();
-    const {name: tagName, slug: tagSlug } = blogPostDetails.tagsCollection.items[0];
-    const moreBlogs = await GetSimilarBlogs({slug: tagSlug, excludeSlug: slug});
-    blogPostDetails.moreFromTagName = tagName;
-    blogPostDetails.moreBlogs = moreBlogs;
-    blogPostDetails.mostPopular = mostPopularBlogs;
-    return blogPostDetails;
-}
+const GetBlogPost = async ({ slug, isPreview }: Props) => {
+  const blogPostId = await getBlogPostId({ slug, isPreview });
+  const blogPostDetails = await getBlogPostDetails({ blogPostId, isPreview });
+  const mostPopularBlogs = await getMostPopularBlogs();
+  const {
+    name: tagName,
+    slug: tagSlug,
+  } = blogPostDetails.tagsCollection.items[0];
+  const moreBlogs = await GetSimilarBlogs({ slug: tagSlug, excludeSlug: slug });
+  blogPostDetails.moreFromTagName = tagName;
+  blogPostDetails.moreBlogs = moreBlogs;
+  blogPostDetails.mostPopular = mostPopularBlogs;
+  return blogPostDetails;
+};
 
 export default GetBlogPost;
