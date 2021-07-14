167bbcb3b443a739230b383c019e0c888d4bd72e
Nick Gottlieb <ngottlieb1@gmail.com>
2021-07-14 15:09:53 -0600

    Update Form.tsx

--- src/components/pages/request-demo/Form.tsx
+++ src/components/pages/request-demo/Form.tsx
@@ -146,11 +146,11 @@ const Form = () => {
                     onBlur={handleBlur}
                   >
                     <option value="" disabled></option>
-                    <option value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
-                    <option value="option4">Option 4</option>
-                    <option value="option5">Option 5</option>
+                    <option value="Less than 100,000/month">Option 1</option>
+                    <option value="100,000-500,000/month">Option 2</option>
+                    <option value="500,000-1,000,000/month">Option 3</option>
+                    <option value="1,000,000-10,000,000/month">Option 4</option>
+                    <option value="More than 10,000,000/month">Option 5</option>
                   </Select>
                   <FormErrorMessage>
                     {errors.notificationVolume}
